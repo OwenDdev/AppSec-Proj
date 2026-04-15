f2d67676a66b7a72a16322b85618ec207c5b60f1
OwenDdev <porewen3848@conestogac.on.ca>
2026-04-14 22:37:36 -0400

    Login 1.5
    Login functionality improved setup to send data to Api.

--- SecureApp/front-end/src/Login.jsx
+++ SecureApp/front-end/src/Login.jsx
@@ -1,28 +1,59 @@
+import { useState } from "react";
+
 function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    function Login(e){
-        const UserName ="filler";
+    async function handleLogin(e){
 
-        // hashed in caase message intercepted when sen to Api
-        const Pasword = "filler";
+        e.preventDefault();
+        console.log(username);
+        console.log(password);
 
-        //code to send information to Api 
+         //code to send information to Api 
+        try {
+            const response = await fetch("http://localhost:5000/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            });
 
+            // store response form Api
+            const data = await response.json();
+            console.log(data);
+        }
+        catch (error) {
+        console.error("Error connecting to API:", error);
+        }
     }
+
+
+    function handlePassword(e){
+        setPassword(e.target.value);
+    }
+    function handleUsername(e){
+        setUsername(e.target.value);
+    }
+
     return(
     <>  
         <h2>AppSec</h2>
 
-        <form>
+        <form onSubmit={handleLogin}>
             <p>UserName:</p>
-            <input type="text"></input>
+            <input type="text" onChange={handleUsername}/>
 
             <p>Password:</p>
-            <input type="password"></input>
+            <input type="password" onChange={handlePassword}/>
 
             <br/>
 
-            <button onClick={Login}>Login</button>
+            <button type="submit">Login</button>
         </form>
     </>
     );
